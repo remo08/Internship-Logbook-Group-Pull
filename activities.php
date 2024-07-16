000d0b37dc84e244452e8958629459e263db7e53
Richard Remo Ali <161082943+remo08@users.noreply.github.com>
2024-07-16 13:45:36 +0300

    Add files via upload

---
+++ activities.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+<style type="text/css">
+<!--
+.style1 {font-family: Georgia, "Times New Roman", Times, serif}
+-->
+</style>
+</head>
+
+<body>
+<h3 align="center" class="style1">Daily Detailed Description of Work</h3>
+<form id="form1" name="form1" method="post" action="">
+  <table width="80%" border="0.5" align="center">
+    <tr>
+      <td>Training Week: </td>
+      <td><input type="text" name="textfield" /></td>
+    </tr>
+    <tr>
+      <td>Day and Date: </td>
+      <td><input type="text" name="textfield2" /></td>
+    </tr>
+  </table>
+</form>
+<form id="form2" name="form2" method="post" action="">
+  <table width="80%" border="1" align="center">
+    <tr>
+      <td colspan="3"><div align="center">
+        <h3 class="style1">Sequence of the Activities / Tasks performed for the job given on the particular day </h3>
+      </div></td>
+    </tr>
+    <tr>
+      <td width="3%"><strong>No</strong></td>
+      <td width="52%"><strong>Activities / Tasks </strong></td>
+      <td width="45%"><strong>Tool, Equipment, etc used </strong></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td><p>That day's Activities / Tasks: </p>
+      <p>
+        <input type="text" name="textfield3" />
+      </p></td>
+      <td><input type="text" name="textfield36" /></td>
+    </tr>
+    <tr>
+      <td height="25">&nbsp;</td>
+      <td><p>Lessions Learnt: </p>
+      <p>
+        <input type="text" name="textfield32" />
+      </p></td>
+      <td><input type="text" name="textfield37" /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td><p>That day's problems / challenges faced while performing your duties: </p>
+      <p>
+        <input type="text" name="textfield33" />
+      </p></td>
+      <td><input type="text" name="textfield38" /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td><p>Student's recommendation on the above problem / challenge on that particular day: </p>
+      <p>
+        <input type="text" name="textfield34" />
+      </p></td>
+      <td><input type="text" name="textfield39" /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td><p>Field Supervisor's Comments for that particular day: </p>
+      <p>
+        <input type="text" name="textfield35" />
+      </p></td>
+      <td><input type="text" name="textfield310" /></td>
+    </tr>
+  </table>
+</form>
+<form id="form3" name="form3" method="post" action="">
+  <table width="80%" border="0.5" align="center">
+    <tr>
+      <td>Student's Date:</td>
+      <td><input type="text" name="textfield311" /></td>
+    </tr>
+    <tr>
+      <td>Field Supervisor Review Date: </td>
+      <td><input type="text" name="textfield312" /></td>
+    </tr>
+  </table>
+</form>
+<p>&nbsp; </p>
+</body>
+
+</html>
